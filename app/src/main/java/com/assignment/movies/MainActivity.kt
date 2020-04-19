12c2975ac58345c0bde4c7fe1161555026bdf5d3
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2020-04-20 02:54:48 +0530

    Model Classes And Retrofit InterFaces Created

--- app/src/main/java/com/assignment/movies/MainActivity.kt
+++ app/src/main/java/com/assignment/movies/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.assignment.movies
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var recycler: RecyclerView? = null
+    var list:List<Movies>?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler = findViewById(R.id.recyclerView)
+        recycler?.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recycler?.adapter = list?.let { RecyclerAdapter(this, it) }
     }
-
 }
--- app/src/main/java/com/assignment/movies/MoviesCallTopRated.kt
+++ app/src/main/java/com/assignment/movies/MoviesCallTopRated.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MoviesCallTopRated {
-    @GET("movie/top_rated")
+    @GET("/movie/top_rated")
     fun getTopRated(
         @Query("page") pagesize: Int,
         @Query("language") language: String?,
