33f6256fe758fb46d76d20dde7a22d73fc6670ed
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2020-04-20 03:15:36 +0530

    Data Classes Were Added From Api

--- app/src/main/java/com/assignment/movies/MainActivity.kt
+++ app/src/main/java/com/assignment/movies/MainActivity.kt
@@ -7,12 +7,11 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private var recycler: RecyclerView? = null
-    var list:List<Movies>?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler = findViewById(R.id.recyclerView)
-        recycler?.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recycler?.adapter = list?.let { RecyclerAdapter(this, it) }
+
     }
 }
---
+++ app/src/main/java/com/assignment/movies/dataUpComing/Dates.kt
@@ -0,0 +1,9 @@
+package com.assignment.movies.dataUpComing
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
