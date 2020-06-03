1c7c23db328c232c700da85129df679a937b2e26
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2020-06-03 22:03:54 +0530

    Gradle Updated

--- app/src/main/java/com/assignment/movies/fragments/TopRated.kt
+++ app/src/main/java/com/assignment/movies/fragments/TopRated.kt
@@ -1,5 +1,7 @@
 package com.assignment.movies.fragments
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
